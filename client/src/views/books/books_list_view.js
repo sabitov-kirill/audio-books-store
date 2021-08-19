3da183e9f00e61c2912b59bf8b82ca4d9377fab6
DS2-github <smirnov.daniil.ds2@gmail.com>
2021-08-19 22:53:36 +0300

    remove odd triangle bracket

--- client/src/views/books/books_list_view.js
+++ client/src/views/books/books_list_view.js
@@ -19,7 +19,7 @@ export default function BooksListView(props) {
     return (
         <Row xs={2} sm={3} lg={4} xl={5} xxl={6}>
             <Col>
-                {props.books.length !== 0
+                {props.books.empty
                     ? props.books.map((book) => {
                         return <BookView
                             key={`todo-${book.id}`}
