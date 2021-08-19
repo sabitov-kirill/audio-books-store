33bc08254fb92308c2a6fae7addf0e6ce953721d
sabitov-kirill <sabkirul@gmail.com>
2021-08-19 21:05:42 +0300

    Branches merge commit.

--- client/src/views/app_view.js
+++ client/src/views/app_view.js
@@ -16,7 +16,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 // Application pages routes
 const pagesRoutes = [
-    { path: '/', Component: React.lazy(() => import('../controllers/books/books_controller')) },
+    { path: '/', Component: React.lazy(() => import('../views/books/main_page')) },
     { path: '/login', Component: React.lazy(() => import('../controllers/user/login_form_controller')) },
     { path: '/reg', Component: React.lazy(() => import('../controllers/user/registration-form-controller')) },
     { path: '/auth', Component: React.lazy(() => import('../views/user/auth_form_view')) }, 
--- client/src/views/books/books_list_view.js
+++ client/src/views/books/books_list_view.js
@@ -20,74 +20,6 @@ export default function BooksListView(props) {
     useEffect(() => props.initBookStorage );
 
     return (
-<<<<<<< HEAD
-        <Container>
-            <InputGroup className="mb-3">
-                <FormControl type="text"
-                             placeholder="Search..."
-                             onChange={onSearchInput}/>
-                <Button variant="outline-secondary"
-                        type='Submit'
-                        onClick={()=>{}}
-                        id="search-button">
-                    <Search />
-                </Button>
-            </InputGroup>
-            <Nav inline="true">
-                <NavDropdown title={`Sorted by: ${props.sort}`} id={"sort"}
-                             onSelect={(eventKey) => handleSelect(eventKey, props.sorting)}>
-                    <NavDropdown.Item
-                        eventKey="New first"
-                        active={props.sort === "New first"}
-                    >
-                        New first
-                    </NavDropdown.Item>
-                    <NavDropdown.Item
-                        eventKey="Old first"
-                        active={props.sort === "Old first"}
-                    >
-                        Old first
-                    </NavDropdown.Item>
-                    <NavDropdown.Item
-                        eventKey="From A to Z"
-                        active={props.sort === "From A to Z"}>
-                        From A to Z
-                    </NavDropdown.Item>
-                    <NavDropdown.Item
-                        eventKey="From Z to A"
-                        active={props.sort === "From Z to A"}>
-                        From Z to A
-                    </NavDropdown.Item>
-                </NavDropdown>
-                <NavDropdown title={`Filters: ${props.filters}`} id={"filter"}
-                             onSelect={(eventKey) => handleSelect(eventKey, props.filter)}>
-                    <NavDropdown.Item
-                        eventKey="All"
-                        active={props.filters === "All"}>
-                        All
-                    </NavDropdown.Item>
-                    <NavDropdown.Item
-                        eventKey="Owned"
-                        active={props.filters === "Owned"}>
-                        Owned
-                    </NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
-            <Row xs={2} sm={3} lg={4} xl={5} xxl={6}>
-                <Col>
-                    {props.books.length !== 0
-                        ? props.books.map((book) => {
-                            return <BookView
-                                key={`todo-${book.id}`}
-                                book={book}
-                                select={() => props.visitBookPage(book)}
-                            />;
-                        })
-                        : ""}
-                </Col>
-            </Row>
-        </Container>
-=======
         <Row xs={2} sm={3} lg={4} xl={5} xxl={6}>
             <Col>
                 {props.books.length !== 0
@@ -101,6 +33,5 @@ export default function BooksListView(props) {
                     : ""}
             </Col>
         </Row>
->>>>>>> 1fac74a5b06f4cd8591139480a872d325d57289a
     );
 };
