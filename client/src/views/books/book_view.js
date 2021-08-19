1a790d0dce3d159d70bf3568456dce6dc9074817
DS2-github <smirnov.daniil.ds2@gmail.com>
2021-08-19 20:52:31 +0300

    remove mistake in onClice event

--- client/src/views/books/book_view.js
+++ client/src/views/books/book_view.js
@@ -13,7 +13,7 @@ import Card from "react-bootstrap/Card";
 
 export default function BookView(props) {
     return (
-        <Card onClick={props.select()}>
+        <Card onClick={props.select}>
             <Card.Header>{props.book.title}</Card.Header>
             <Card.Img variant="top" src={props.book.imgPath} />
         </Card>
